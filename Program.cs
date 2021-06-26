c30bbaa418e440c4a37137290bb088fb05c1ab41
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 23:03:53 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNasceu;
+            int anoAtual;
+            int podeVotar;
+
+
+            Console.WriteLine("Digite o ano em que você nasceu: ");
+            anoNasceu = int.Parse(Console.ReadLine());
+
+            anoAtual = DateTime.Now.Year;
+
+            podeVotar = anoAtual - anoNasceu;
+
+            if (podeVotar < 16)
+            {
+                Console.WriteLine("Você não pode votar esse ano");
+            }
+
+            else
+            {
+                Console.WriteLine("Você pode votar esse ano");
+            }
+        }
+    }
+}
